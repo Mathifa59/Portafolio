e45f2b90716c8a6448daf3fd6d48d1ea0b0728ba
Mathifaa519 <mathiwen519@gmail.com>
2026-01-07 09:39:27 -0500

    add images

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import { projects, Project } from "@/data/projects"; 
-import { ExternalLink, Github, ArrowRight, Rocket } from "lucide-react"; // Importamos Rocket para el icono de startup
+import { ExternalLink, ArrowRight, Rocket } from "lucide-react"; // Importamos Rocket para el icono de startup
 import Image from "next/image";
 
 export default function Projects() {
